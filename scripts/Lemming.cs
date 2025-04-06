3c00f28f3ff85913f15b83e65832dd6d9bacea29
Rodion Iafarov <rodion.iafarov@jetbrains.com>
2025-04-06 14:32:11 +0200

    Update project to godot 4

--- scripts/Lemming.cs
+++ scripts/Lemming.cs
@@ -5,24 +5,24 @@ using Dictionary = Godot.Collections.Dictionary;
 using Array = Godot.Collections.Array;
 
 
-public class Lemming : KinematicBody2D
+public partial class Lemming : CharacterBody2D
 {
-     
-    public Vector2 velocity = new Vector2(0,0);
-    [Export] public int gravity  = 800;
-    
-    public override void _PhysicsProcess(float delta)
-    {  
-        velocity.y += gravity;
-        var collision = MoveAndCollide(velocity * delta);
-        if(collision != null)
-        {
-            if(collision.Collider.HasMethod("collide"))
-            {
-                velocity.x *= -100;    
-            }
-        }
-    }  
-    
-    
+	 
+	public Vector2 velocity = new Vector2(0,0);
+	[Export] public int gravity  = 800;
+	
+	public override void _PhysicsProcess(double delta)
+	{  
+		velocity.Y += gravity;
+		var collision = MoveAndCollide(velocity * (float) delta);
+		if(collision != null)
+		{
+			if(collision.GetCollider().HasMethod("collide"))
+			{
+				velocity.X *= -100;    
+			}
+		}
+	}  
+	
+	
 }
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,80 +1,89 @@
-using System;
-using System.Linq;
 using Godot;
-using Dictionary = Godot.Collections.Dictionary;
-using Array = Godot.Collections.Array;
+using System;
 
-
-public class Player : KinematicBody2D
+public partial class Player : CharacterBody2D
 {
-     
-    public const int MAX_SPEED = 600;
-    
-    [Export] public bool jumping  = false;
-    [Export] public int move_speed  = 250;
-    [Export] public int jump_force  = 700;
-    [Export] public int gravity  = 1800;
-    
-    public Vector2 speed  = new Vector2();
-    public Vector2 velocity  = new Vector2();
-    public Vector2 snap_vector  = new Vector2(0, 32);
-    public ulong prev_vert_wall  = 0;
-    
-    public void ProcessJump()
-    {  
-        speed.y = -jump_force;
-        jumping = true;    
-    }
-    
-    public override void _PhysicsProcess(float delta)
-    {  
-        var direction_x  = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-        
-        speed.x = direction_x * move_speed    ;
-        speed.y += gravity * delta;
-        // Limit maximum speed
-        speed.x = Mathf.Clamp(speed.x, -MAX_SPEED, MAX_SPEED);
-        speed.y = Mathf.Clamp(speed.y, -MAX_SPEED, MAX_SPEED);
-    
-        velocity = MoveAndSlideWithSnap( speed, jumping ? speed : snap_vector, Vector2.Up, false, 4, Mathf.Deg2Rad(46), true );
-        
-        if(IsOnFloor())
-        {
-                velocity.y = 0;
-                jumping = false;
-                prev_vert_wall = 0    ;
-                
-        }
-        if(Input.IsActionJustPressed("jump"))        
-        {
-            // Is on floor, allow jumping    
-            if(IsOnFloor())
-            {
-                ProcessJump();
-            // Allow wall jumps only when in air already
-            }
-            else if(IsOnWall() && jumping)
-            {
-                // Do !allow wall jumps from the same wall
-                foreach(var i in Enumerable.Range(0, GetSlideCount()))
-                {
-                    var collision = GetSlideCollision(i);
-                    if(collision.ColliderId != prev_vert_wall && collision.Collider.IsClass("StaticBody2D"))    
-                    {
-                        ProcessJump();
-                        prev_vert_wall = collision.ColliderId;
-                        break;
-    
-                    }
-                }
-            }
-        }
-    }
-    
-    public void collide()
-    {  
-    
-    
-    }
-    
+	public const int MAX_SPEED = 600;
+
+	[Export] public bool jumping = false;
+	[Export] public int move_speed = 250;
+	[Export] public int jump_force = 700;
+	[Export] public int gravity = 1800;
+	
+	public ulong prev_vert_wall = 0;
+
+	public Vector2 ProcessJump(Vector2 velocity)
+	{
+		velocity.Y = -jump_force;
+		jumping = true;
+		return velocity;
+	}
+
+	public override void _Ready()
+	{
+		
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		UpDirection = Vector2.Up;
+		//WallMinSlideAngle = Mathf.DegToRad(80);
+		MotionMode = MotionModeEnum.Grounded;
+		//FloorMaxAngle = Mathf.DegToRad(80);
+		Vector2 velocity = Velocity;
+		
+		// Handle horizontal movement
+		float direction_x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+		velocity.X = direction_x * move_speed;
+
+		// Apply gravity only when not on the floor
+		if (!IsOnFloor())
+		{
+			velocity.Y += gravity * (float)delta;
+		}
+
+		// Clamp velocity to MAX_SPEED
+		velocity.X = Mathf.Clamp(velocity.X, -MAX_SPEED, MAX_SPEED);
+		velocity.Y = Mathf.Clamp(velocity.Y, -MAX_SPEED, MAX_SPEED);
+
+		// Ensure we correctly detect the floor
+		if (IsOnFloor())
+		{
+			velocity.Y = 0;
+			jumping = false;
+			prev_vert_wall = 0;
+		}
+
+		// Handle jumping logic
+		if (Input.IsActionJustPressed("jump"))
+		{
+			if (IsOnFloor())
+			{
+				velocity = ProcessJump(velocity);
+			}
+			else if (IsOnWall() && jumping)
+			{
+				// Wall jump logic using GetLastSlideCollision()
+				var collision = GetLastSlideCollision();
+				if (collision != null && collision.GetCollider() is StaticBody2D collider)
+				{
+					ulong colliderId = collider.GetInstanceId();
+					if (colliderId != prev_vert_wall)
+					{
+						velocity = ProcessJump(velocity);
+						prev_vert_wall = colliderId;
+					}
+				}
+			}
+		}
+		// Set the velocity and handle movement
+		Velocity = velocity;
+		
+		// Use MoveAndSlide correctly for collision detection and floor check
+		MoveAndSlide();
+	}
+
+	public void Collide()
+	{
+		// Placeholder for additional collision logic
+	}
 }
